42f0ae0a2b416c0fb2c5a11c4c0c9382a7086660
shoot21 <nikitaivashenkov01@icloud.com>
2021-10-25 01:36:46 +0300

    DowloadImageApp

--- DownloadImageApp/DownloadImageApp/ViewController.swift
+++ DownloadImageApp/DownloadImageApp/ViewController.swift
@@ -9,14 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var getImageButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        
+        activityIndicator.isHidden = true
+        activityIndicator.hidesWhenStopped = true
     }
-
-
+    
+    @IBAction func getImagePressed(_ sender: Any) {
+        
+        label.isHidden = true
+        getImageButton.isEnabled = false
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        guard let url = URL(string: "https://i1.7fon.org/thumb/c129919.jpg") else { return }
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.activityIndicator.stopAnimating()
+                    self.imageView.image = image
+                }
+            }
+        }.resume()
+    }
 }
 
