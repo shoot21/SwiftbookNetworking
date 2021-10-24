4c76ab56bbc083f3dd1e781aa26c96b12a6b709b
shoot21 <nikitaivashenkov01@icloud.com>
2021-10-25 00:01:32 +0300

    first commit

---
+++ DownloadImageApp/DownloadImageApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DownloadImageApp
+//
+//  Created by Никита Иващенков on 25/10/2021.
+//  Copyright © 2021 Никита Иващенков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+}
+
